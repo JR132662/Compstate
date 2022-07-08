abbe5cc89b7f23be0a15d4f3d375b63ddd8cd5e2
JR132662 <cooljon08@gmail.com>
2022-07-08 00:41:23 +0000

    saving

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,28 @@
-import React, {useState,useEffect} from "react"
+import React, {useState} from "react"
 
 function Home(){
 	const [ color, setColor] = useState("red");
 
-	return (
+	const Cycle = () => {
+		if (color=="red"){
+			setColor("green")
+		}else if (color=="green"){
+			setColor("yellow")
+		}else{
+			setColor("red")
+		};
+			
+		
+	}
+	  
+
+return(
 		<div className="container"><h1>Traffic Light</h1>
 			<div className="boxcontainer">
 				<div onClick={()=>setColor("green")} className={(color=="green") ? "box green selected":"box green"}></div>
 				<div onClick={()=>setColor("yellow")} className={(color=="yellow") ? "box yellow selected":"box yellow"}></div>
 				<div onClick={()=>setColor("red")} className={(color=="red")?"box red selected":"box red"}></div>
+				<button onClick={Cycle} className="cycle-button"><p>Click me</p></button>
 			</div>
 		</div>
 	)
